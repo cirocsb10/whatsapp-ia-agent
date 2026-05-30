392d5d8f02dd1c1efee1f848f264f7445cfdfb4a
cirocsb10 <ciroviski@gmail.com>
2026-05-30 17:26:56 -0300

    fix(api): remove import redundante de PrismaModule no ClerkWebhookModule

--- apps/api/src/modules/clerk/clerk-webhook.module.ts
+++ apps/api/src/modules/clerk/clerk-webhook.module.ts
@@ -1,10 +1,8 @@
 import { Module } from "@nestjs/common";
 import { ClerkWebhookController } from "./clerk-webhook.controller";
 import { ClerkWebhookService } from "./clerk-webhook.service";
-import { PrismaModule } from "../../common/prisma/prisma.module";
 
 @Module({
-  imports: [PrismaModule],
   controllers: [ClerkWebhookController],
   providers: [ClerkWebhookService],
 })
